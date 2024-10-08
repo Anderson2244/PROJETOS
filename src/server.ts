b9d0e68c32fd6bfed93da95ca67ee2a3caa8cb90
Anderson2244 <andersonsouzzaa2@gmail.com>
2024-10-08 17:19:15 -0300

    update

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,12 @@
 //importando o express
 import express,{Request, Response} from 'express'
+import path from 'path'
 //importando o arquivo index.ts das rotas
 import mainRoutes from './routes/index'
 //usando express
 const server = express()
+//configuração da pasta public
+server.use(express.static(path.join(__dirname,'../public')))
 //inserindo a rota no servidor
 server.use(mainRoutes)
 //página não encontrada
